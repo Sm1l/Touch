bf215d54ea56e225ea326e92fdbea0273be190ec
Sm1le <onekonstantin@gmail.com>
2024-10-01 23:37:25 +0300

    runningLines

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+});
